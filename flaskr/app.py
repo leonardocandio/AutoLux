34dc2f93457097d082d4f9ce434e9c4fe3b6e62f
RenatoCernades0107 <renato.cernades@utec.edu.pe>
2022-04-05 23:14:03 -0500

    se agrego contrasena de postgres como argumento - funcional

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,14 +1,19 @@
+import sys
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes.home import menu
 from utils.db import db
 
+# recogemos la contraseña
+password = f'{sys.argv[1]}'
+
 app = Flask(__name__)
 
 #ver que llaves tiene app.config 
-print(app.config)
+# print(app.config)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5432@localhost:5432/carsapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{password}@localhost:5432/carsapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_mapping({
     'ENV': 'development',
